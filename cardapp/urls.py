a207c14d3ee3dab73c45cd209d63adfbec5ef5ab
Anthony Lopez <Antl21292@gmail.com>
2023-09-12 00:41:55 -0400

    added path for media

--- cardapp/urls.py
+++ cardapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import CardList, CardListDetail
+from django.conf.urls.static import static
+from django.conf import settings
+from .views import CardList, CardListDetail, card_image
 
 urlpatterns = [
     path("", CardList.as_view(), name="card_list"),
     path("<int:pk>/", CardListDetail.as_view(), name="card_list_detail"),
-    # path("media/images/")
+    path("media/<int:card_id>", card_image, name="card.html"),
     # TODO: create url for card image
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
