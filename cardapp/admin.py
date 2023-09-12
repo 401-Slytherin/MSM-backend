8081abd1689408442599406548c0a56eef95d00c
Anthony Lopez <Antl21292@gmail.com>
2023-09-11 23:13:19 -0400

    added CardAdmin

--- cardapp/admin.py
+++ cardapp/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import CardModel
 
+
+class CardAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+
 # Register your models here.
-admin.site.register(CardModel)
+admin.site.register(CardModel, CardAdmin)
